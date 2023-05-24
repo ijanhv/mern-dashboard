287841f5ef681963b6f75205c58020e9d0efb013
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-05-24 22:02:55 +0530

    feat(app): fixed errors

---
+++ frontend/src/state/index.js
@@ -0,0 +1,19 @@
+ import { createSlice } from "@reduxjs/toolkit";
+ 
+ const initialState = {
+    mode: "dark",
+    userId: "63701cc1f03239bf700000e"
+};
+
+export const global = createSlice({
+    name: "global",
+    initialState,
+    reducers: {
+        setMode: (state, action) => {
+            state.mode = state.mode === 'light' ? 'dark' : 'light';
+        }
+    }
+});
+
+export const { setMode } = global.actions;
+export default global.reducer;
